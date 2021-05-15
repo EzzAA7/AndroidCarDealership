36aca615c663cb9b347f0bc4a8bb363dd56676a6
Ezz <izzabuassab@hotmail.com>
2021-05-16 01:34:30 +0300

    added recycleView implementation instead of the cursor linearView to YourFavouritesFragment

--- app/src/main/java/com/example/cardealer/ui/yourFavourites/YourFavouritesFragment.java
+++ app/src/main/java/com/example/cardealer/ui/yourFavourites/YourFavouritesFragment.java
@@ -7,16 +7,26 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.cardealer.R;
+import com.example.cardealer.controller.CarAdapter;
+import com.example.cardealer.controller.DataBaseHelper;
+import com.example.cardealer.model.Car;
+
+import java.util.ArrayList;
 
 public class YourFavouritesFragment extends Fragment {
 
     private YourFavouritesViewModel mViewModel;
+    RecyclerView recyclerView;
+    CarAdapter carAdapter;
+    ArrayList<Car> cars;
 
     public static YourFavouritesFragment newInstance() {
         return new YourFavouritesFragment();
@@ -25,7 +35,22 @@ public class YourFavouritesFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.your_favourites_fragment, container, false);
+        View view = inflater.inflate(R.layout.your_favourites_fragment, container, false);
+        DataBaseHelper dataBaseHelper =new DataBaseHelper(getActivity(),"PROJ", null,1);
+//        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.layout);
+//        linearLayout.removeAllViews();
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        cars = dataBaseHelper.getAllCarsList();
+
+        carAdapter = new CarAdapter(cars, getActivity());
+        recyclerView.setAdapter(carAdapter);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(llm);
+
+        return view;
     }
 
     @Override
