085dd182db9b4c2a1c6f826a3d9f14eeb7e73182
Ezz <izzabuassab@hotmail.com>
2021-05-16 17:14:38 +0300

    added registerUser & checkUser funcs for Auth

--- app/src/main/java/com/example/cardealer/controller/DataBaseHelper.java
+++ app/src/main/java/com/example/cardealer/controller/DataBaseHelper.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.cardealer.model.Car;
+import com.example.cardealer.model.User;
 
 import java.util.ArrayList;
 
@@ -31,9 +32,48 @@ public class DataBaseHelper extends android.database.sqlite.SQLiteOpenHelper {
         sqLiteDatabase.insert("CAR", null, contentValues);
     }
 
+    public boolean registerUser(String firstName, String lastName, String email, String password, String gender, String country, String city, String phoneNumber) {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("FNAME", firstName);
+        contentValues.put("LNAME",
+                lastName);
+        contentValues.put("EMAIL", email);
+        contentValues.put("PASSWORD", password);
+        contentValues.put("GENDER", gender);
+        contentValues.put("COUNTRY", country);
+        contentValues.put("CITY", city);
+        contentValues.put("PHONENUMBER", phoneNumber);
+        long result = sqLiteDatabase.insert("USER", null, contentValues);
+
+        if(result == -1){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkUser(String email, String password) {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        String []  columns = {"ID"};
+        String selections = "EMAIL" + "=?" + " and " + "PASSWORD" + "=?";
+        String [] selectionargs = { email, password};
+
+        Cursor cursor = sqLiteDatabase.query("USER", columns, selections, selectionargs, null, null, null);
+        int count = cursor.getCount();
+        sqLiteDatabase.close();
+        cursor.close();
+        if(count > 0){
+            return true;
+        }
+        return false;
+    }
+
+
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("CREATE TABLE CAR(YEAR TEXT, MAKE TEXT,MODEL TEXT, DISTANCE TEXT, PRICE TEXT, ACCIDENTS BOOLEAN, OFFERS BOOLEAN ) ");
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS USER(ID INTEGER PRIMARY KEY AUTOINCREMENT, FNAME TEXT,LNAME TEXT, EMAIL TEXT, PASSWORD TEXT, GENDER TEXT, COUNTRY TEXT, CITY TEXT, PHONENUMBER TEXT ) ");
     }
 
     @Override
@@ -63,17 +103,6 @@ public class DataBaseHelper extends android.database.sqlite.SQLiteOpenHelper {
                 String price = cursor.getString(4);
                 Boolean accidents = (cursor.getInt(5) == 1);
                 Boolean offers = (cursor.getInt(6) == 1);
-//                String x = cursor.getString(5);
-//                Boolean accidents;
-//                if(x.equals("true")) {
-//                    accidents = true;
-//                }else accidents = false;
-//                String y = cursor.getString(6);
-//                Boolean offers;
-//                if(y.equals("true")) {
-//                    offers = true;
-//                }else offers = false;
-
                 Car car = new Car(year,make, model, distance, price, accidents, offers);
                 System.out.println(car.toString());
                 cars.add(car);
