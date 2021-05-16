16fcffdfbfbacb5ac810e643d7093d2ea163ba8b
Ezz <izzabuassab@hotmail.com>
2021-05-16 23:17:14 +0300

    menu nav header now uses session email

--- app/src/main/java/com/example/cardealer/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/cardealer/ui/home/HomeFragment.java
@@ -4,18 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.cardealer.R;
-import com.example.cardealer.ui.specialOffers.SpecialOffersFragment;
-import com.example.cardealer.ui.specialOffers.SpecialOffersViewModel;
-import com.example.cardealer.view.SharedPrefManager;
 
 public class HomeFragment extends Fragment {
 
--- app/src/main/java/com/example/cardealer/view/NavActivity.java
+++ app/src/main/java/com/example/cardealer/view/NavActivity.java
@@ -3,10 +3,10 @@ package com.example.cardealer.view;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.example.cardealer.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.example.cardealer.service.SharedPrefManager;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -20,6 +20,8 @@ import androidx.appcompat.widget.Toolbar;
 public class NavActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    TextView tvProfileName, tvProfileEmail;
+    SharedPrefManager sharedPrefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,17 @@ public class NavActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+
+        sharedPrefManager = SharedPrefManager.getInstance(this);
+        View headerView = navigationView.getHeaderView(0);
+        TextView navEmail = (TextView) headerView.findViewById(R.id.tvProfileEmail);
+        String em = sharedPrefManager.readString("Session","noValue");
+        navEmail.setText(em);
+
+        // TODO: ADD GET NAME FROM DB
+        //        TextView navName = (TextView) headerView.findViewById(R.id.tvProfileName);
+        //        navName.setText(name);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
