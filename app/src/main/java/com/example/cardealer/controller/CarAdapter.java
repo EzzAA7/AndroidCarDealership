413c747651c4f80c33863f304f05de80e3a3c7fd
Ezz <izzabuassab@hotmail.com>
2021-05-17 23:56:14 +0300

    implemented reserve button in carAdapter

--- app/src/main/java/com/example/cardealer/controller/CarAdapter.java
+++ app/src/main/java/com/example/cardealer/controller/CarAdapter.java
@@ -13,13 +13,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cardealer.R;
 import com.example.cardealer.model.Car;
+import com.example.cardealer.model.User;
+import com.example.cardealer.service.SharedPrefManager;
+import com.example.cardealer.view.SignUpActivity;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class CarAdapter extends RecyclerView.Adapter<CarAdapter.CarVH>{
 
     ArrayList<Car> cars;
     Context context;
+    SharedPrefManager sharedPrefManager;
 
     public CarAdapter(ArrayList<Car> cars, Context context) {
         this.cars = cars;
@@ -42,10 +48,39 @@ public class CarAdapter extends RecyclerView.Adapter<CarAdapter.CarVH>{
         holder.tvDistance.setText(c.getDistance());
         holder.tvPrice.setText(c.getPrice() + " Shekel" );
 
+        DataBaseHelper dataBaseHelper =new DataBaseHelper(context,"PROJ", null,1);
+
         holder.buttonReserve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, (c.getMake() + " " + c.getModel() + " " + c.getYear()) + " has been reserved", Toast.LENGTH_SHORT).show();
+                // get current session's user
+                sharedPrefManager = SharedPrefManager.getInstance(context);
+                String email = sharedPrefManager.readString("Session","noValue");
+                User currentUser = dataBaseHelper.getUser(email);
+
+                // get current formated date
+                LocalDateTime myDateObj = LocalDateTime.now();
+                DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                String formattedDate = myDateObj.format(myFormatObj);
+
+                // once everything is ready we can create a reservation
+                boolean result = dataBaseHelper.createReservation(holder.tvModelInfo.getText().toString(),
+                        holder.tvDistance.getText().toString(),
+                        c.getPrice(),
+                        (currentUser.getfName()+ " " + currentUser.getlName()),
+                        currentUser.getPhoneNumber(),
+                        formattedDate);
+
+                // check if db registeration action succeeded
+                if(result){
+
+                    // send cofirmation toast
+                    Toast.makeText(context, ("The " + c.getMake() + " " + c.getModel() + " " + c.getYear()) + " has been reserved", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(context, "Reservation Failed, try Again!", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
