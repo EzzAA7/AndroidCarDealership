d3651eddfa606e61b1d0c52ab065c6e65c91422f
Ezz <izzabuassab@hotmail.com>
2021-05-18 02:09:10 +0300

    added favourite & reserve button implementation in SpecialOffer

--- app/src/main/java/com/example/cardealer/controller/SpecialOfferAdapter.java
+++ app/src/main/java/com/example/cardealer/controller/SpecialOfferAdapter.java
@@ -13,13 +13,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cardealer.R;
 import com.example.cardealer.model.Car;
+import com.example.cardealer.model.User;
+import com.example.cardealer.service.SharedPrefManager;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class SpecialOfferAdapter extends RecyclerView.Adapter<SpecialOfferAdapter.SpecialOfferVH>{
 
     ArrayList<Car> cars;
     Context context;
+    SharedPrefManager sharedPrefManager;
 
     public SpecialOfferAdapter(ArrayList<Car> cars, Context context) {
         this.cars = cars;
@@ -39,19 +44,84 @@ public class SpecialOfferAdapter extends RecyclerView.Adapter<SpecialOfferAdapte
     public void onBindViewHolder(@NonNull SpecialOfferVH holder, int position) {
         Car c = cars.get(position);
         holder.tvInfo.setText((c.getMake() + " " + c.getModel() + " " + c.getYear()));
+        holder.tvDist.setText(c.getDistance());
+
+//        holder.buttonReserve.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(context, (c.getMake() + " " + c.getModel() + " " + c.getYear()) + " has been reserved", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        holder.buttonFav.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(context, (c.getMake() + " " + c.getModel() + " " + c.getYear()) + " has been added to favourites", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
         holder.buttonReserve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, (c.getMake() + " " + c.getModel() + " " + c.getYear()) + " has been reserved", Toast.LENGTH_SHORT).show();
+
+                DataBaseHelper dataBaseHelper =new DataBaseHelper(context,"PROJ", null,1);
+
+                // get current session's user
+                sharedPrefManager = SharedPrefManager.getInstance(context);
+                String email = sharedPrefManager.readString("Session","noValue");
+                User currentUser = dataBaseHelper.getUser(email);
+
+                // get current formated date
+                LocalDateTime myDateObj = LocalDateTime.now();
+                DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                String formattedDate = myDateObj.format(myFormatObj);
+
+                // once everything is ready we can create a reservation
+                boolean result = dataBaseHelper.createReservation(holder.tvInfo.getText().toString(),
+                        holder.tvDist.getText().toString(),
+                        c.getPrice(),
+                        (currentUser.getfName()+ " " + currentUser.getlName()),
+                        currentUser.getPhoneNumber(), currentUser.getEmail(),
+                        formattedDate);
+
+                // check if db reservation action succeeded
+                if(result){
+
+                    // send cofirmation toast
+                    Toast.makeText(context, ("The " + c.getMake() + " " + c.getModel() + " " + c.getYear()) + " has been reserved", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(context, "Reservation Failed, try Again!", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
         holder.buttonFav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, (c.getMake() + " " + c.getModel() + " " + c.getYear()) + " has been added to favourites", Toast.LENGTH_SHORT).show();
-            }
+                DataBaseHelper dataBaseHelper =new DataBaseHelper(context,"PROJ", null,1);
+
+                // get current session's user
+                sharedPrefManager = SharedPrefManager.getInstance(context);
+                String email = sharedPrefManager.readString("Session","noValue");
+                User currentUser = dataBaseHelper.getUser(email);
+
+                // once everything is ready we can create a favourite
+                boolean result = dataBaseHelper.createFavourite(holder.tvInfo.getText().toString(),
+                        holder.tvDist.getText().toString(),
+                        c.getPrice(),
+                        (currentUser.getfName()+ " " + currentUser.getlName()),
+                        currentUser.getPhoneNumber(), currentUser.getEmail());
+
+                // check if db favourite action succeeded
+                if(result){
+                    // send cofirmation toast
+                    Toast.makeText(context, ("The " + c.getMake() + " " + c.getModel() + " " + c.getYear())+ " has been favourited ❤", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(context, "Favourite Failed, try Again!", Toast.LENGTH_SHORT).show();
+                }            }
         });
     }
 
@@ -62,13 +132,14 @@ public class SpecialOfferAdapter extends RecyclerView.Adapter<SpecialOfferAdapte
 
     class SpecialOfferVH extends RecyclerView.ViewHolder{
 
-        TextView tvInfo;
+        TextView tvInfo, tvDist;
         Button buttonReserve, buttonFav;
 
         public SpecialOfferVH(@NonNull View itemView) {
             super(itemView);
 
             tvInfo = itemView.findViewById(R.id.tvInfo);
+            tvDist = itemView.findViewById(R.id.tvCarDistSO);
             buttonReserve = itemView.findViewById(R.id.btnReserveSO);
             buttonFav = itemView.findViewById(R.id.btnFavSO);
         }
