d1309854a4e8eecfd07993592c567d2ad21c3bda
Ezz <izzabuassab@hotmail.com>
2021-05-19 01:27:39 +0300

    created image model

---
+++ app/src/main/java/com/example/cardealer/model/Image.java
@@ -0,0 +1,38 @@
+package com.example.cardealer.model;
+
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+public class Image {
+    private static final float PREFERRED_WIDTH = 75;
+    private static final float PREFERRED_HEIGHT = 100;
+    private String title;
+    private Bitmap image;
+
+    public Image(String title, Bitmap image) {
+        this.title = title;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+}
