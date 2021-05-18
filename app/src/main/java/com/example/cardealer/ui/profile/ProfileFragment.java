c9da82c81d05ccb4dd60da8a2ab0c0b24a596a62
Ezz <izzabuassab@hotmail.com>
2021-05-18 17:41:00 +0300

    seperated area code from number + added validation

--- app/src/main/java/com/example/cardealer/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/cardealer/ui/profile/ProfileFragment.java
@@ -17,12 +17,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.cardealer.R;
 import com.example.cardealer.controller.DataBaseHelper;
 import com.example.cardealer.model.User;
 import com.example.cardealer.service.SharedPrefManager;
+import com.example.cardealer.view.SignUpActivity;
 
 import org.mindrot.jbcrypt.BCrypt;
 
@@ -32,6 +34,7 @@ public class ProfileFragment extends Fragment {
 
     private ProfileViewModel mViewModel;
     EditText etFirstName, etLastName, etPassword, etConfirmPassword, etPhoneNumber;
+    TextView tvAreaCodeProfile;
     Button editCustomerButton;
     LinearLayout linearLayout;
     SharedPrefManager sharedPrefManager;
@@ -54,6 +57,7 @@ public class ProfileFragment extends Fragment {
         etPassword = (EditText)view.findViewById(R.id.editTextPasswordCustEdit);
         etConfirmPassword = (EditText)view.findViewById(R.id.editTextConfirmPasswordCusEdit);
         etPhoneNumber = (EditText)view.findViewById(R.id.editText_phoneCustEdit);
+        tvAreaCodeProfile = (TextView) view.findViewById(R.id.tvAreaCodeProfile);
         editCustomerButton = (Button) view.findViewById(R.id.btnCustEdit);
         ImageView img = (ImageView) view.findViewById(R.id.imageViewProfile);
         img.setImageResource(R.drawable.defualt_profile);
@@ -72,7 +76,10 @@ public class ProfileFragment extends Fragment {
         etLastName.setText(currentUser.getlName());
         etPassword.setText(currentUser.getPassword());
         etConfirmPassword.setText(currentUser.getPassword());
-        etPhoneNumber.setText(currentUser.getPhoneNumber());
+
+        String phone = currentUser.getPhoneNumber();
+        tvAreaCodeProfile.setText(phone.substring(0,5));
+        etPhoneNumber.setText(phone.substring(5));
 
         editCustomerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,29 +96,63 @@ public class ProfileFragment extends Fragment {
                 // if not empty then check for validation
                 else{
 
-                    // if passwords match then we can send register action to db
-                    if(etPassword.getText().toString().equals(etConfirmPassword.getText().toString())){
+                    // calculate validation characters
+                    String pw = etPassword.getText().toString();
+                    int pwNumOfChars = pw.length();
+                    int digitCount = 0;
+                    int letterCount = 0;
+                    int scCount = 0;
 
-                        String password = etPassword.getText().toString();
-                        String pw_hash = BCrypt.hashpw(password, BCrypt.gensalt());
-
-                        boolean var = dataBaseHelper.updateUser(etFirstName.getText().toString(),
-                                etLastName.getText().toString(), email,
-                                pw_hash,
-                                etPhoneNumber.getText().toString());
-
-                        // check if db registeration action succeeded
-                        if(var){
-                            Toast.makeText(getActivity(), "User edited successfully!", Toast.LENGTH_SHORT).show();
+                    for( int i = 0; i < pwNumOfChars; i++ )
+                    {
+                        if (Character.isDigit(pw.charAt(i))) {
+                            digitCount++;
                         }
-                        else {
-                            Toast.makeText(getActivity(), "Editing Failed, try Again!", Toast.LENGTH_SHORT).show();
+
+                        if (Character.isAlphabetic(pw.charAt(i))) {
+                            letterCount++;
+                        }
+
+                        if (!Character.isDigit(pw.charAt(i)) && !Character.isAlphabetic(pw.charAt(i))
+                                && !Character.isWhitespace(pw.charAt(i))) {
+                            scCount++;
                         }
                     }
 
-                    // if passwords dont match then alert the user
-                    else{
-                        showAlertDialogNotEqual(linearLayout);
+                    if(etFirstName.getText().toString().length() < 3 ||
+                            etLastName.getText().toString().length() < 3 || pwNumOfChars < 5 ||
+                            digitCount < 1 || letterCount < 1 || scCount < 1 ){
+
+                        showAlertDialogWrongValid(linearLayout);
+                    }
+
+                    else {
+
+                        // if passwords match then we can send register action to db
+                        if(etPassword.getText().toString().equals(etConfirmPassword.getText().toString())){
+
+                            String password = etPassword.getText().toString();
+                            String pw_hash = BCrypt.hashpw(password, BCrypt.gensalt());
+                            String phone = etPhoneNumber.getText().toString();
+
+                            boolean var = dataBaseHelper.updateUser(etFirstName.getText().toString(),
+                                    etLastName.getText().toString(), email,
+                                    pw_hash,
+                                    (tvAreaCodeProfile.getText().toString() + phone));
+
+                            // check if db registration action succeeded
+                            if(var){
+                                Toast.makeText(getActivity(), "User edited successfully!", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(getActivity(), "Editing Failed, try Again!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        // if passwords dont match then alert the user
+                        else{
+                            showAlertDialogNotEqual(linearLayout);
+                        }
                     }
 
                 }
@@ -148,6 +189,20 @@ public class ProfileFragment extends Fragment {
         AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
         alert.setTitle("Unsuccessful Action");
         alert.setMessage("Some fields are empty");
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                Toast.makeText(getActivity(), "Try Again! :)", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alert.create().show();
+    }
+
+    // the alert func for wrong validation in a field
+    public void showAlertDialogWrongValid(View v) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+        alert.setTitle("Unsuccessful Action");
+        alert.setMessage("Some fields are not valid");
         alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int i) {
