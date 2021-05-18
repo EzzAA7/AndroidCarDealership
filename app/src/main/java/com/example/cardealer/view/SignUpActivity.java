82a788c8791f22fa3b2a29bc524ede287d77f631
Ezz <izzabuassab@hotmail.com>
2021-05-18 03:14:37 +0300

    updated validation

--- app/src/main/java/com/example/cardealer/view/SignUpActivity.java
+++ app/src/main/java/com/example/cardealer/view/SignUpActivity.java
@@ -93,34 +93,67 @@ public class SignUpActivity extends AppCompatActivity {
                 // if not empty then check for validation
                 else{
 
-                    // if passwords match then we can send register action to db
-                    if(etPassword.getText().toString().equals(etConfirmPassword.getText().toString())){
+                    String pw = etPassword.getText().toString();
+                    int pwNumOfChars = pw.length();
+                    int digitCount = 0;
+                    int letterCount = 0;
+                    int scCount = 0;
 
-                        String password = etPassword.getText().toString();
-                        String pw_hash = BCrypt.hashpw(password, BCrypt.gensalt());
-
-                        boolean var = dataBaseHelper.registerUser(etFirstName.getText().toString(),
-                                etLastName.getText().toString(), etEmail.getText().toString(),
-                                pw_hash, gender.getSelectedItem().toString(),
-                                countries.getSelectedItem().toString(),
-                                cities.getSelectedItem().toString(),
-                                etPhoneNumber.getText().toString(),
-                                "customer"
-                                );
-
-                        // check if db registeration action succeeded
-                        if(var){
-                            Toast.makeText(SignUpActivity.this, "User registered successfully!", Toast.LENGTH_SHORT).show();
+                    for( int i = 0; i < pwNumOfChars; i++ )
+                    {
+                        if (Character.isDigit(pw.charAt(i))) {
+                            digitCount++;
                         }
-                        else {
-                            Toast.makeText(SignUpActivity.this, "Registration Failed, try Again!", Toast.LENGTH_SHORT).show();
 
+                        if (Character.isAlphabetic(pw.charAt(i))) {
+                            letterCount++;
+                        }
+
+                        if (!Character.isDigit(pw.charAt(i)) && !Character.isAlphabetic(pw.charAt(i))
+                                && !Character.isWhitespace(pw.charAt(i))) {
+                            scCount++;
                         }
                     }
 
-                    // if passwords dont match then alert the user
+                    if(etFirstName.getText().toString().length() < 3 ||
+                            etLastName.getText().toString().length() < 3 || pwNumOfChars < 5 ||
+                                    digitCount < 1 || letterCount < 1 || scCount < 1 ){
+
+                        showAlertDialogWrongValid(linearLayout);
+                    }
+
                     else{
-                        showAlertDialogNotEqual(linearLayout);
+
+                        // if passwords match then we can send register action to db
+                        if(etPassword.getText().toString().equals(etConfirmPassword.getText().toString())){
+
+                            String password = etPassword.getText().toString();
+                            String pw_hash = BCrypt.hashpw(password, BCrypt.gensalt());
+
+                            boolean var = dataBaseHelper.registerUser(etFirstName.getText().toString(),
+                                    etLastName.getText().toString(), etEmail.getText().toString(),
+                                    pw_hash, gender.getSelectedItem().toString(),
+                                    countries.getSelectedItem().toString(),
+                                    cities.getSelectedItem().toString(),
+                                    etPhoneNumber.getText().toString(),
+                                    "customer"
+                            );
+
+                            // check if db registeration action succeeded
+                            if(var){
+                                Toast.makeText(SignUpActivity.this, "User registered successfully!", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(SignUpActivity.this, "Registration Failed, try Again!", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+
+                        // if passwords dont match then alert the user
+                        else{
+                            showAlertDialogNotEqual(linearLayout);
+                        }
+
                     }
 
                 }
@@ -157,6 +190,20 @@ public class SignUpActivity extends AppCompatActivity {
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setTitle("Unsuccessful Action");
         alert.setMessage("Some fields are empty");
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                Toast.makeText(SignUpActivity.this, "Try Again! :)", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alert.create().show();
+    }
+
+    // the alert func for wrong validation in a field
+    public void showAlertDialogWrongValid(View v) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Unsuccessful Action");
+        alert.setMessage("Some fields are not valid");
         alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int i) {
